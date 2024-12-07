d75838529e485b981706a428e16022e9eade432f
xnts0 <81151912+xnts0@users.noreply.github.com>
2024-12-07 15:09:17 +0300

    add arraylist

--- mini_project/src/main/java/com/fx/TaskManager.java
+++ mini_project/src/main/java/com/fx/TaskManager.java
@@ -1,8 +1,14 @@
 package com.fx;
 
+import java.util.ArrayList;
+
 public class TaskManager extends Task {
+        private ArrayList<Task> tasks;
+
     TaskManager(String name, boolean isDone) {
         super(name, isDone);
+        tasks = new ArrayList<>();
+
     }
 
     // Im Abdullah I will do add task
